24e8f173f3a3cf602f2e1ab0e1ee0e166e7c1616
Tyler Nijmeh <tylernij@gmail.com>
2026-07-06 17:51:35 -0500

    Fix paths
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- scripts/build-x264.sh
+++ scripts/build-x264.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Fetch the patched x264 for YP3. Usage:
-#   build_x264 <prefix> <patch_file> [workdir]
+#   build_x264 <prefix> [workdir]
 build_x264() {
     prefix="$1"
     workdir="${2:-/tmp/x264-build}"
